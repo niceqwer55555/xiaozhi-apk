82e518aae43ad9604de5715fd77a83a448dfc4da
347921622@qq.com <niceqwer55555>
2025-02-26 18:52:57 +0800

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.lhht.xiaozhi.ai"
-        minSdk = 29
+        minSdk = 30
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
